dddd77c4c23e4e07bff873802630c2c88c4bb1e3
Şilan HK <silanharmanci.9@gmail.com>
2024-05-30 19:37:20 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let toplam =0;
+let addScore = document.getElementsByClassName("score-a");
+
+function addFunctionA(){
+    toplam = toplam+1;
+    toplam++;
+    addScore.innerHTML = "Skor: " +toplam;
+    
+}
